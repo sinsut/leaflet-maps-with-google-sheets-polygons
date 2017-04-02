ccd0ac56750a991fb89672bf13b1eaac562d1af0
sinsut <sin.sut@gmail.com>
2017-04-03 02:37:43 +0900

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JpRcgAX73xnKeUntOA4Dm7AwOWlD18KhUYzC0X29G-o';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JpRcgAX73xnKeUntOA4Dm7AwOWlD18KhUYzC0X29G-o/edit#gid=0';
